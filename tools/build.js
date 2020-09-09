eebcaa9095fb8e8fd779856d900f589aefdfebae
Vallabh Kansagara <vrkansagara@gmail.com>
2020-09-09 23:23:25 +0530

    adding project init.

---
+++ tools/build.js
@@ -0,0 +1,12 @@
+{
+    "appDir": "./assets/js",
+    "mainConfigFile": "./assets/js/app.js",
+    "dir": "../www-built",
+    // List the modules that will be optimized.
+    "modules": [
+        {
+            // module names are relative to baseUrl
+            "name": "app"
+        }
+    ]
+}
